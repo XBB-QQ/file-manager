1829b21798a6b736284891408c8a77e08a449971
王肖强 <13951730076@163.com>
2026-05-21 19:32:07 +0800

    再不行我不搞了

--- android/app/src/main/java/com/filemanager/app/MainActivity.java
+++ android/app/src/main/java/com/filemanager/app/MainActivity.java
@@ -4,8 +4,12 @@ import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onStart() {
-        super.onStart();
-        registerPlugin(StoragePermissionPlugin.class);
+    public void onCreate() {
+        super.onCreate();
+        try {
+            registerPlugin(StoragePermissionPlugin.class);
+        } catch (Exception e) {
+            // Plugin may already be registered after cap sync
+        }
     }
 }
