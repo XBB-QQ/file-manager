ecdf761c532beb118db45ee15a67dd19d6b00158
王肖强 <13951730076@163.com>
2026-05-22 17:59:07 +0800

    build(android: 移除ci中手动注册插件的步骤
    将手动注册插件的ci脚本，改为直接修改MainActivity代码来添加插件注册逻辑，不再需要在ci中动态修改代码

--- android/app/src/main/java/com/filemanager/app/MainActivity.java
+++ android/app/src/main/java/com/filemanager/app/MainActivity.java
@@ -1,11 +1,12 @@
 package com.filemanager.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate() {
-        super.onCreate();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         try {
             registerPlugin(StoragePermissionPlugin.class);
         } catch (Exception e) {
